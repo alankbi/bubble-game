e7b164b9e12b206e2f1a0fdd967095630fdfddc2
Alan-Bi <alan.bi326@gmail.com>
2018-06-30 16:17:28 -0700

    Sharpened text, added proper animation

--- Assets/LoadAnimation.cs
+++ Assets/LoadAnimation.cs
@@ -5,21 +5,21 @@ using UnityEngine;
 public class LoadAnimation : MonoBehaviour {
 
 	private Object[] frames;
-	private const int framesPerSecond = 40;
+	private const int framesPerSecond = 10;
 
 	public GameObject animation;
 	private SpriteRenderer sr;
 
 	// Use this for initialization
 	void Start () {
-		frames = Resources.LoadAll ("tuffy_frames", typeof(Sprite));
+		frames = Resources.LoadAll ("tuffy_frames1", typeof(Sprite));
 		sr = animation.GetComponent<SpriteRenderer> ();
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		var index = (int) (Time.time * framesPerSecond) % frames.Length;
-		sr.sprite.texture = (Sprite) frames[index];
+		sr.sprite = (Sprite) frames[index];
 		Debug.Log (index);
 	}
 }
---
+++ Assets/TextSharpener.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TextSharpener : MonoBehaviour {
+
+	public Text[] text;
+	private const int Scale = 2;
+
+	// Use this for initialization
+	void Start () {
+		foreach (var t in text) {
+			t.fontSize *= Scale;
+			t.transform.localScale /= Scale;
+
+			t.horizontalOverflow = HorizontalWrapMode.Overflow;
+			t.verticalOverflow = VerticalWrapMode.Overflow;
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
