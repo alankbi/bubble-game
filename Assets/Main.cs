2a2876545358eaaf80372338f5720eb856e8c4e9
Alan-Bi <alan.bi326@gmail.com>
2018-07-29 17:01:55 -0700

    Remove moving elements from main menu page

--- Assets/Main.cs
+++ Assets/Main.cs
@@ -5,56 +5,20 @@ using UnityEngine;
 [RequireComponent(typeof(AudioSource))]
 public class Main : MonoBehaviour {
 
-	public Transform canvas;
-
-	private List<GameObject> bubbles;
-	private List<Bubble> bubbleObjects;
-
-	private Rect canvasDimensions;
-	private float time;
-
 	private AudioSource audioSource;
 	private SoundPlayer soundPlayer;
 
 	// Use this for initialization
 	void Start () {
-		canvasDimensions = canvas.GetComponent<RectTransform> ().rect;
-		time = 0;
-
 		audioSource = GetComponent<AudioSource> ();
 		soundPlayer = new SoundPlayer(audioSource);
 		soundPlayer.PlayStartSound ();
 
-		bubbles = new List<GameObject>();
-		bubbleObjects = new List<Bubble> ();
-
-		for (int i = 0; i < 300; i++) {
-			bubbleObjects.Add(new Bubble ("Bubble", 
-				new Vector2 ((float)(Random.value * canvasDimensions.width - canvasDimensions.width / 2), 
-					(float)(Random.value * canvasDimensions.height * -2 - canvasDimensions.height / 2)), 
-				new Vector2 ((float)(Random.value * 5 - 2.5), (float)(Random.value * 8 + 1)), 
-				canvas, 
-				(int) (Random.value * 5 + 6)));
-			bubbles.Add(bubbleObjects[i].bubble);
-		}
-
-		soundPlayer.PlayBackgroundBubbling ();
+		soundPlayer.PlayBackgroundMusic ();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		time += Time.deltaTime;
-
-		for (int i = 0; i < bubbles.Count; i++) {
-			var testBody = bubbles[i].GetComponent<Rigidbody2D> ();
-			var vel = testBody.velocity;
-			vel.x = bubbleObjects [i].Amplitude * Mathf.Sin (time * bubbleObjects [i].Period);
-			testBody.velocity = vel;
-
-			if (bubbles[i].transform.localPosition.y > canvasDimensions.height / 2 + 20) {
-				bubbles[i].transform.localPosition = 
-					new Vector2 ((float)(Random.value * canvasDimensions.width - canvasDimensions.width / 2), -5 * canvasDimensions.height / 2);
-			}
-		}
+		
 	}
 }
